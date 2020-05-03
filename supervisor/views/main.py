0029096776ea9a0f2a6470932b7aa2e19abb079e
varun1414 <naik.varun99@gmail.com>
2020-05-03 15:36:25 +0530

    dgm start

--- supervisor/views/main.py
+++ supervisor/views/main.py
@@ -180,6 +180,7 @@ def sent(request):
     temp.approval_date=date
     temp.approval_time=time
     temp.unit_incharge_approval='NO'
+    temp.status="PENDING"
     print("temp")
     # currdate = date.today()
     temp.save()
